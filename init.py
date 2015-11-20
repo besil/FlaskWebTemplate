e61a4e4b24f7abc529b2c1c3dc14d4c8b2b3b8b3
Silvio Bernardinello <silvio.bernardinello@gmail.com>
2015-11-20 11:15:38 +0100

    Bug fixing

--- init.py
+++ init.py
@@ -10,10 +10,10 @@ if __name__ == '__main__':
 
     project_name = args.project_name
 
+    call("cd TemplateProject/webapp/static/lib; bash install_libs.sh", shell=True)
     call("rm -rf .git", shell=True)
     call("rm LICENSE", shell=True)
     call("rm README.md", shell=True)
     call("cd TemplateProject/", shell=True)
-    call("TemplateProject/webapp/static/lib; bash install_libs.sh", project_name)
     os.rename("TemplateProject", project_name)
     os.remove("init.py")
